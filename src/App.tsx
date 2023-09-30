c3d472b029a06afe03cc779dd884ee7da93aca9c
marko <krznarm26@gmail.com>
2023-09-30 23:39:53 +0200

    Added components, ESLint rules and API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
 import './scss/style.scss';
 import placeholder from './assets/placeholder.svg';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+
 function App() {
+	const [data, setData] = useState<any>(null);
+
+	useEffect(() => {
+		const headers = {
+			'x-rapidapi-host': 'v3.football.api-sports.io',
+			'x-rapidapi-key': '7ba7477f0c4de0f9fe92bc247995d041',
+		};
+
+		axios
+			// eslint-disable-next-line prettier/prettier
+			.get('https://v3.football.api-sports.io/standings?league=39&season=2023', { headers })
+			.then((response) => {
+				setData(response.data);
+			})
+			.catch((error) => {
+				// eslint-disable-next-line no-console
+				console.error('Error:', error);
+			});
+	}, []);
+
 	return (
 		<div className="container">
 			<div className="content-block content-block-intro">
@@ -45,36 +69,18 @@ function App() {
 							<th>Name</th>
 							<th>Points</th>
 						</tr>
-						<tr>
-							<td>1</td>
-							<td>Arsenal</td>
-							<td>90</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Arsenal</td>
-							<td>90</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Arsenal</td>
-							<td>90</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Arsenal</td>
-							<td>90</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Arsenal</td>
-							<td>90</td>
-						</tr>
-						<tr>
-							<td>1</td>
-							<td>Arsenal</td>
-							<td>90</td>
-						</tr>
+						{data &&
+							data?.response[0].league.standings[0].map(
+								(team: any) => {
+									return (
+										<tr key={team.team.id}>
+											<td>{team.rank}</td>
+											<td>{team.team.name}</td>
+											<td>{team.points}</td>
+										</tr>
+									);
+								}
+							)}
 					</tbody>
 				</table>
 			</div>
