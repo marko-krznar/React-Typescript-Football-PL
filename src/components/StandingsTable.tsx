14ef58dbd3815ce3ad1b34e995839bd7a288c379
marko <krznarm26@gmail.com>
2023-10-02 22:15:05 +0200

    Added types to StandingsTable component

--- src/components/StandingsTable.tsx
+++ src/components/StandingsTable.tsx
@@ -1,19 +1,41 @@
-function StandingsTable(standings: any) {
+export interface TeamProp {
+	rank?: string;
+	points?: string;
+	team?: {
+		id?: string;
+		name?: string;
+	};
+	home?: {
+		played?: number;
+	};
+}
+
+export interface StandingsProp {
+	data: Array<TeamProp>;
+}
+
+function StandingsTable(standings: StandingsProp) {
+	const { data } = standings;
+	
 	return (
 		<table>
 			<tbody>
 				<tr>
 					<th>Position</th>
-					<th>Name</th>
+					<th>Games Played</th>
 					<th>Points</th>
+					<th>Name</th>
 				</tr>
-				{standings.data &&
-					standings.data.map((team: any) => {
+				{data &&
+					data.map((teamItem: TeamProp) => {
+						const { rank, team, points, home } = teamItem;
+
 						return (
-							<tr key={team.team.id}>
-								<td>{team.rank}</td>
-								<td>{team.team.name}</td>
-								<td>{team.points}</td>
+							<tr key={team?.id}>
+								<td>{rank}</td>
+								<td>{team?.name}</td>
+								<td>{home?.played}</td>
+								<td>{points}</td>
 							</tr>
 						);
 					})}
