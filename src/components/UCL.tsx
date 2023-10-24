7607922db91299dc72509f2cb0b61b2888b1dc84
marko <krznarm26@gmail.com>
2023-10-24 21:46:46 +0200

    Added component for UCL competition

--- src/components/UCL.tsx
+++ src/components/UCL.tsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react';
-import './scss/style.scss';
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import axios from 'axios';
-import StandingsTable from './components/StandingsTable';
-import Fixture from './components/Fixture';
-import UCL from './components/UCL';
+import StandingsTable from './StandingsTable';
+import Fixture from './Fixture';
 
-function App() {
+function UCL() {
 	const [data, setData] = useState<any>(null);
 	const [lastFixture, setLastFixture] = useState<any>(null);
 	const [nextFixture, setNextFixture] = useState<any>(null);
@@ -20,7 +18,7 @@ function App() {
 
 		axios
 			// eslint-disable-next-line prettier/prettier
-			.get('https://v3.football.api-sports.io/standings?league=39&season=2023', { headers })
+			.get('https://v3.football.api-sports.io/standings?league=2&season=2023', { headers })
 			.then((response) => {
 				setData(response.data);
 			})
@@ -38,7 +36,7 @@ function App() {
 
 		axios
 			// eslint-disable-next-line prettier/prettier
-			.get('https://v3.football.api-sports.io/fixtures?league=39&season=2023&team=42&last=1', { headers })
+			.get('https://v3.football.api-sports.io/fixtures?league=2&season=2023&team=42&last=1', { headers })
 			.then((response) => {
 				setLastFixture(response.data);
 			})
@@ -56,7 +54,7 @@ function App() {
 
 		axios
 			// eslint-disable-next-line prettier/prettier
-			.get('https://v3.football.api-sports.io/fixtures?league=39&season=2023&team=42&next=1', { headers })
+			.get('https://v3.football.api-sports.io/fixtures?league=2&season=2023&team=42&next=1', { headers })
 			.then((response) => {
 				setNextFixture(response.data);
 			})
@@ -67,30 +65,12 @@ function App() {
 	}, []);
 
 	return (
-		<div className="container">
-			<div className="section-dude">
-				<div className="content-block content-block-intro">
-					<h1 className="heading">Premier League Results</h1>
-					<p className="text">
-						Welcome to the Premier League Results app, meticulously
-						crafted to enhance your knowledge and understanding of one
-						of the most prestigious football leagues in the world.
-						Designed with a focus on expanding your horizons, this app
-						offers a wealth of information, from match outcomes and
-						insightful player analyses to captivating historical data
-					</p>
-					<p className="text">
-						Embrace the opportunity to delve deeper into the intricacies
-						of the Premier League, empowering yourself with a
-						comprehensive understanding of the game and its captivating
-						evolution. Get ready to embark on a journey of knowledge and
-						discovery with our immersive app experience.
-					</p>
-				</div>
-				{lastFixture?.response[0] && (
+		<>
+			{lastFixture?.response[0] && (
+				<div className="section-dude">
 					<div className="content-block-result">
 						<Fixture
-							date={lastFixture.response[0].fixture.date}
+							date={lastFixture.response[0]?.fixture?.date}
 							homeTeamName={lastFixture.response[0].teams.home.name}
 							homeTeamLogo={lastFixture.response[0].teams.home.logo}
 							homeTeamGoals={lastFixture.response[0].goals.home}
@@ -106,16 +86,15 @@ function App() {
 							awayTeamLogo={nextFixture.response[0].teams.away.logo}
 						/>
 					</div>
-				)}
-			</div>
-			{data && (
-				<div className="content-block content-block-standings-table">
-					<StandingsTable data={data?.response[0]?.league?.standings[0]} />
 				</div>
 			)}
-			<UCL />
-		</div>
-	);
+			{data && (
+				<div className="content-block content-block-standings-table">
+					<StandingsTable data={data?.response[0]?.league?.standings[1]} />
+				</div>
+			)}
+		</>
+	)
 }
 
-export default App;
+export default UCL
